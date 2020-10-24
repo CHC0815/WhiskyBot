c1bfc0e66f19f6280d6031210f51774b0722182b
Conrad Carl <coheca0815@gmail.com>
2020-10-24 21:10:25 +0200

    design test

--- whiskySite/index.js
+++ whiskySite/index.js
@@ -4,7 +4,6 @@ const app = express()
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    console.log("request")
     let loggedIn = true;
     if(!loggedIn)
         res.render('login', {google_url: 'google.com'})
