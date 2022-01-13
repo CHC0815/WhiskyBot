f9755f4a1e21c4b141c6f8066eb1d55ed55325fc
Conrad Heinrich Carl <coheca0815@gmail.com>
2022-01-13 12:21:57 +0100

    clean up

--- whiskySite/splitwise.js
+++ whiskySite/splitwise.js
@@ -1,13 +1,20 @@
 var AuthApi = require('splitwise-node')
 
-//api key 0sRaXQPTyrWKHlNw2vsQ4PcwsQr4TLjhxojCXK4y
 
 var userOAuthToken, userOAuthTokenSecret
-var authApi = new AuthApi("sEkI7SS9dU7M5Mq7gfB2wH4KsuQsYPbrZ8vnb5H4", "IPEiZjZaLHTENfZMl8AbJTpcWA9ewBHFRxPIUprQ")
-var userAuthUrl = authApi.getOAuthRequestToken().then(({token, secret}) => {
+var authApi = new AuthApi("", "")
+var userAuthUrl = authApi.getOAuthRequestToken().then(({
+    token,
+    secret
+}) => {
     [userOAuthToken, userOAuthTokenSecret] = [token, secret]
     return apigateway.getUserAuthorisationUrl(token)
 })
 
 
-module.exports = { userAuthUrl, userOAuthToken, userOAuthTokenSecret, authApi }
+module.exports = {
+    userAuthUrl,
+    userOAuthToken,
+    userOAuthTokenSecret,
+    authApi
+}
